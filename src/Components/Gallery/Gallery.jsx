4fabe5fd7b8a65f5f784d19643cab0614a369584
Alfiya Heba <alfiyaheba10@gmail.com>
2025-06-16 21:54:32 +0530

    updatde gallery

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './Gallery.css';
-import picture1 from '../../assets/picture1.jpg';
-import picture2 from '../../assets/picture2.jpg';
-import picture3 from '../../assets/picture3.jpg';
-import picture4 from '../../assets/picture4.jpg';
+import picture1 from '../../assets/Picture1.jpg';
+import picture2 from '../../assets/Picture2.jpg';
+import picture3 from '../../assets/Picture3.jpg';
+import picture4 from '../../assets/Picture4.jpg';
 import picture5 from '../../assets/picture5.jpg';
 import picture6 from '../../assets/picture6.jpg';
 import picture7 from '../../assets/picture7.jpg';
